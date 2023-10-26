724934e45844df79c76f72cd6f256de2b21996dd
dominik <dominik.barukcic@fer.hr>
2023-10-26 12:51:07 +0200

    Adding the project files to git repo

---
+++ src/main/java/hr/fer/progi/posterized/PosterizedApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.posterized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosterizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosterizedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/fer/progi/posterized/PosterizedApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.progi.posterized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosterizedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
