b8d8f2b668aeba44e8364a9c57a12b987999d70b
tea2403 <tt54151@fer.hr>
2023-10-28 23:48:49 +0200

    spring.profiles.active=basic-security i odkomentiran WebSecurityBasic

--- posterized-backend/src/main/java/hr/fer/progi/posterized/rest/WebSecurityBasic.java
+++ posterized-backend/src/main/java/hr/fer/progi/posterized/rest/WebSecurityBasic.java
@@ -15,9 +15,9 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-//@Profile("basic-security")
+@Profile("basic-security")
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = false)
-public class WebSecurity {
+public class WebSecurityBasic {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
@@ -26,13 +26,13 @@ public class WebSecurity {
         http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
-    //@Bean
-    //@Order(Ordered.HIGHEST_PRECEDENCE)
-    //public SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
-        //http.securityMatcher(PathRequest.toH2Console());
-        //http.csrf(AbstractHttpConfigurer::disable);
-        //http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
-        //return http.build();
-    //}
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain h2ConsoleSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher(PathRequest.toH2Console());
+        http.csrf(AbstractHttpConfigurer::disable);
+        http.headers((headers) -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        return http.build();
+    }
 
 }
