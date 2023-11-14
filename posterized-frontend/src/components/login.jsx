17364477313c6199ac843196e55126ad61b4e241
nikabozic <nika.bozic@fer.hr>
2023-11-14 01:21:15 +0100

    frontend login page

--- posterized-frontend/src/components/login.jsx
+++ posterized-frontend/src/components/login.jsx
@@ -1,12 +1,48 @@
-import React from 'react';
 
-const Login = () => {
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [lozinka, setLozinka] = useState('');
+
+    async function authenticate(event) {
+        event.preventDefault();
+        try {
+            console.log("Logging in with:", email, lozinka);
+        } catch (error) {
+            console.error("Login error:", error);
+        }
+    }
+
     return (
         <div>
             <h2>Login Page</h2>
-            {/* Your login form and content */}
+            <form onSubmit={authenticate}>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="lozinka">Lozinka:</label>
+                    <input
+                        type="password"
+                        id="lozinka"
+                        value={lozinka}
+                        onChange={(e) => setLozinka(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <button type="submit">Prijavi se</button>
+                </div>
+            </form>
         </div>
     );
-};
+}
 
 export default Login;
