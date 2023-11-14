b0ccd80a8c55bdb2e191c37b6408d21feff5b04d
tea2403 <tt54151@fer.hr>
2023-11-14 09:41:30 +0100

    link na login

--- posterized-frontend/src/components/Register.jsx
+++ posterized-frontend/src/components/Register.jsx
@@ -1,5 +1,6 @@
     import React, { useState } from 'react';
     import axios from 'axios';
+    import { Link } from 'react-router-dom';
     
     function Register() {
         const [ime, setIme] = useState('');
@@ -22,6 +23,7 @@
                             'Content-Type': 'application/json',
                         },
                     });
+                    alert("Registracija uspješna");
                 } catch(err) {
                     console.error(err);
                     if(err.response.data.message) {
@@ -79,6 +81,9 @@
                         />
                     </div>
                     <button type="submit">Register</button>
+                    <Link to="/login">
+                        <div>Login</div>
+                    </Link>
                 </form>
     
             </div>
