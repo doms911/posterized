35abfdb6cea3d63948ebbf00a7d4749bd8248f54
dominik <dominik.barukcic@fer.hr>
2023-11-16 21:48:30 +0100

    front

--- posterized-frontend/src/setupProxy.js
+++ posterized-frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: 'http://posterized.onrender.com:8080/',
+      target: 'http://localhost:8080/',
       changeOrigin: true,
     })
   );
