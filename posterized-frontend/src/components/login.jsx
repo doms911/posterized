b58d12461c45cc521be1ecaad1fbb684bd9a3bbe
dominik <dominik.barukcic@fer.hr>
2023-11-16 11:13:35 +0100

    dodao linkove za api

--- posterized-frontend/src/components/login.jsx
+++ posterized-frontend/src/components/login.jsx
@@ -18,7 +18,7 @@ function Login(props) {
             },
             body: body,
         };
-        fetch('http://localhost:8080/login', options)
+        fetch('https://posterized-be.onrender.com/login', options)
             .then((response) => {
                 if (response.status === 401) {
                     alert('Pogrešna lozinka');
