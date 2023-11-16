f9dab15ae08c24948eb054c2b6e868bbd431942d
dominik <dominik.barukcic@fer.hr>
2023-11-16 12:32:05 +0100

    dodan proxy

--- posterized-frontend/.eslintrc.cjs
+++ posterized-frontend/.eslintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true,
+    "amd": true,
+    "node": true},
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
