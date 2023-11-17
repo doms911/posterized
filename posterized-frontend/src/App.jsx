ef95d7816ddfcb36f17f9c4e1b090375fa4fdf72
tea2403 <tt54151@fer.hr>
2023-11-17 09:03:00 +0100

    vracam

--- posterized-frontend/src/App.jsx
+++ posterized-frontend/src/App.jsx
@@ -22,7 +22,7 @@ const App = () => {
     };
 
     const handleLogout = () => {
-        fetch("/api/radovi", {
+        fetch("/api/logout", {
             method: 'POST',
             credentials: 'include', 
         })
