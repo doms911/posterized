dc253de7ce1bb76c167d39cad50dac1395fc7cc3
enasamarzija <es53888@fer.hr>
2023-12-05 14:28:41 +0100

    Dodavanje header-a

--- posterized-frontend/src/HomePage.jsx
+++ posterized-frontend/src/HomePage.jsx
@@ -1,31 +1,15 @@
 // HomePage.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Header from './components/Header';
 
 const HomePage = (props) => {
     const isLoggedIn = props.isLoggedIn;
     const onLogout = props.onLogout;
 
-    const username = localStorage.getItem('username'); // Assuming you store the username in localStorage
-
     return (
         <div>
             <h1>Welcome to the Homepage</h1>
-            {isLoggedIn ? (
-                <div>
-                    <p>Pozdrav, {username}! Ulogirani ste u sustav.</p>
-                    <button onClick={onLogout}>Logout</button>
-                </div>
-            ) : (
-                <div>
-                    <Link to="/register">
-                        <button>Register</button>
-                    </Link>
-                    <Link to="/login">
-                        <button>Login</button>
-                    </Link>
-                </div>
-            )}
+            <Header isLoggedIn={isLoggedIn} onLogout={onLogout} />
         </div>
     );
 };
---
+++ posterized-frontend/src/components/Sidebar.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import "./Sidebar.css"
+import { SidebarData } from './SidebarData';
+import { AiOutlineBars } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
+import { IconContext } from 'react-icons';
+const Sidebar  = (props) => {
+
+    const [sidebar, setSidebar] = useState(false);
+
+    const showSidebar = () => setSidebar(!sidebar);
+
+    return (
+        <>
+            <IconContext.Provider value={{ color: '#fff' }}>
+                <div className='navbar'>
+                    <Link to='#' className='menu-bars'>
+                        <AiOutlineBars onClick={showSidebar} />
+                    </Link>
+                </div>
+                <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                    <ul className='nav-menu-items' onClick={showSidebar}>
+                        <li className='navbar-toggle'>
+                            <Link to='#' className='menu-bars'>
+                                <IoClose />
+                            </Link>
+                        </li>
+                        {SidebarData.map((item, index) => {
+                            return (
+                                <li key={index} className={item.cName}>
+                                    <Link to={item.path}>
+                                        {item.icon}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </nav>
+            </IconContext.Provider>
+        </>
+    );
+};
+
+export default Sidebar;
