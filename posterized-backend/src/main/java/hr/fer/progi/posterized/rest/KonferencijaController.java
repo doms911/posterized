a04f27ba522b02be21d26ee5ea57daada2bc883b
kris-chros <kd54293@fer.hr>
2023-12-27 17:35:21 +0100

    losa proba

--- posterized-backend/src/main/java/hr/fer/progi/posterized/rest/KonferencijaController.java
+++ posterized-backend/src/main/java/hr/fer/progi/posterized/rest/KonferencijaController.java
@@ -1,14 +1,17 @@
 package hr.fer.progi.posterized.rest;
 
 import hr.fer.progi.posterized.service.KonferencijaService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import hr.fer.progi.posterized.domain.Konferencija;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 
-@RestController
+/*@RestController*/
+@Controller
 @RequestMapping("")
 public class KonferencijaController {
     @Autowired
@@ -21,5 +24,14 @@ public class KonferencijaController {
     @PostMapping("/pin")
     public Konferencija provjeriPin(@RequestParam("pin") Integer pin){
         return kService.provjeriPin(pin);
+    }
+
+    @GetMapping("/konferencije")
+    public List<Konferencija> dohvatiKonferencije(){
+        return kService.listAll();
+    }
+    @GetMapping("")
+    public String prikazKonf(){
+        return "prikazKonferencije";
     }
 }
