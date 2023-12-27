77decbab7965e371d81fb863c6c9ea2e00868199
tea2403 <tt54151@fer.hr>
2023-12-28 00:07:02 +0100

    vracanje podataka o konf za superadmina frontu

--- posterized-backend/src/main/java/hr/fer/progi/posterized/rest/KonferencijaController.java
+++ posterized-backend/src/main/java/hr/fer/progi/posterized/rest/KonferencijaController.java
@@ -1,14 +1,16 @@
 package hr.fer.progi.posterized.rest;
 
+import hr.fer.progi.posterized.domain.Osoba;
 import hr.fer.progi.posterized.service.KonferencijaService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import hr.fer.progi.posterized.domain.Konferencija;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 
 
 @RestController
@@ -30,8 +32,24 @@ public class KonferencijaController {
     }
 
     @GetMapping("/prikaziSve")
-    public List<Konferencija> prikazKonf() {
-        return kService.listAll();
+    public List<Map<String, String>> prikazKonf() {
+        List<Konferencija> konferencije = kService.listAll();
+        List<Map<String, String>> rezultat = new ArrayList<>();
+
+        for (Konferencija konferencija : konferencije) {
+            Map<String, String> konferencijaMapa = new HashMap<>();
+            konferencijaMapa.put("naziv", konferencija.getNaziv());
+            konferencijaMapa.put("pin", konferencija.getPin().toString());
+
+            Osoba admin = konferencija.getAdminKonf();
+            konferencijaMapa.put("adminIme", admin.getIme());
+            konferencijaMapa.put("adminPrezime", admin.getPrezime());
+            konferencijaMapa.put("adminEmail", admin.getEmail());
+
+            rezultat.add(konferencijaMapa);
+        }
+
+        return rezultat;
     }
 
     /*@GetMapping("")
