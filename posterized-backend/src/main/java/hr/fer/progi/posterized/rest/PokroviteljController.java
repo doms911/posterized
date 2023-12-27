8669406508747d883757d180013776c1a99caea8
tea2403 <tt54151@fer.hr>
2023-12-27 22:23:09 +0100

    pokrovitelj i spremanje slika

---
+++ posterized-backend/src/main/java/hr/fer/progi/posterized/rest/PokroviteljController.java
@@ -0,0 +1,31 @@
+package hr.fer.progi.posterized.rest;
+
+
+import hr.fer.progi.posterized.domain.Pokrovitelj;
+import hr.fer.progi.posterized.service.PokroviteljService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RestController
+//@Controller
+@RequestMapping("/pokrovitelj")
+public class PokroviteljController {
+    @Autowired
+    private PokroviteljService pokrService;
+    @PostMapping("")
+    public Pokrovitelj createPokrovitelj (@RequestParam("url") String url,@RequestParam("naziv") String naziv, @RequestParam("slika") MultipartFile slika) throws IOException {
+        Pokrovitelj pokrovitelj = new Pokrovitelj();
+        pokrovitelj.setUrl(url);
+        pokrovitelj.setNaziv(naziv);
+        return pokrService.createPokrovitelj(pokrovitelj, slika);
+    }
+
+    /*@GetMapping("")
+    public String createAdmin(){
+        return "sponzor";
+    }*/
+}
