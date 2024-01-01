6b7afdef414a8fdd99b78f4849cea55c4b1ea3a3
tea2403 <tt54151@fer.hr>
2024-01-01 22:43:51 +0100

    admincontroller u osobacontroller

--- posterized-backend/src/main/java/hr/fer/progi/posterized/rest/OsobaController.java
+++ posterized-backend/src/main/java/hr/fer/progi/posterized/rest/OsobaController.java
@@ -1,26 +1,32 @@
 package hr.fer.progi.posterized.rest;
 
-import hr.fer.progi.posterized.domain.Osoba;
 import hr.fer.progi.posterized.service.OsobaService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import hr.fer.progi.posterized.domain.Osoba;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 //@Controller
-@RequestMapping("/stvoriAdmina")
-public class AdminController {
+@RequestMapping("/registracija")
+public class OsobaController {
     @Autowired
     private OsobaService oService;
     @PostMapping("")
+    public void createKorisnik(@RequestBody Osoba osoba) {
+        oService.createAdminKorisnik(osoba);
+    }
+
+    @PostMapping("/admin")
     @Secured("superadmin")
-    public void createAdminKorisnik(@RequestBody Osoba osoba) {
+    public void createAdmin(@RequestBody Osoba osoba) {
         String lozinka = osoba.getLozinka();
         oService.createAdminKorisnik(osoba);
         oService.saljiMail(osoba, lozinka);
     }
-    /*@GetMapping("")
+    /*@GetMapping("/admin")
     public String createAdmin(){
         return "stvaranjeAdmina";
     }*/
