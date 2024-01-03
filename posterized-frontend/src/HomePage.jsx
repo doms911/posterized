640c7d8445a4e247b53e51b1e94a8a568cf804e8
tea2403 <tt54151@fer.hr>
2024-01-03 21:01:17 +0100

    proba za firebase

--- posterized-frontend/src/HomePage.jsx
+++ posterized-frontend/src/HomePage.jsx
@@ -44,10 +44,6 @@ const HomePage = (props) => {
                     </div>
                 )}
                 {/* Rest of the main content */}
-                <form action="/api/fotografija/naziv" method="post" enctype="multipart/form-data">
-        <input type="file" name="slike" id="images" accept="image/*" multiple required/>
-        <button type="submit">Upload Images</button>
-    </form>
             </main>
         </div>
     );
